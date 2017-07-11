021daac62f992078eb723b355484d34adde57bef
jtangqt <ajasmineflower@gmail.com>
2017-07-11 03:02:49 -0400

    modified move.hpp a bit

--- project1-checkers/move.hpp
+++ project1-checkers/move.hpp
@@ -2,7 +2,7 @@
 #define MOVE_HPP_
 
 class Move{
-	int curr_x, curr_y, new_x, new_y, num;//num = where it is on the list
+	int curr_x, curr_y, new_x, new_y, num;//which move number is it? (mostly for user to give input)
 	Move *next_move; 
 	public:
 		Move(int, int, int, int, int);
