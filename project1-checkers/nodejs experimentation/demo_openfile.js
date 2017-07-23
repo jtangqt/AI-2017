2e36829231f213f299c89d7bbf30aab386f10fdd
Brian Hong <brian@hongs.me>
2017-07-23 16:30:02 -0400

    cleanup: Removed nodejs directory

--- project1-checkers/nodejs experimentation/demo_openfile.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-
-fs.open('mynewfile2.txt', 'w', function(err, file){
-	if (err) throw err;
-	console.log('Saved!'); 
-	// open method takes a "flag" as a second argument, if the flag is 'w' for 'writing', the specified file is open for writing
-	//if the file does not exist, an empty file is created
-});
