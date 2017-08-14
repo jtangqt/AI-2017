46f4a116f4130c82faeb16cb14d90212391752b9
jtangqt <ajasmineflower@gmail.com>
2017-08-14 14:13:51 -0400

    added some stuff to neural nets project

--- project2-neuralnets/BackProp.m
+++ project2-neuralnets/BackProp.m
@@ -1,7 +1,14 @@
-function neural_net = BackProp(X, y, weights_1, weights_2, epoch, alpha)
+function neural_net = BackProp(X, y, init, epoch, alpha)
 
 n = size(X, 1); 
 m = 0; 
+neural_net = init(2:end, :); 
+
+hidden_nodes = init(1,2); % # of hidden nodes
+output_nodes = init(1,3); % # of output nodes
+weights_inner = init(2:hidden_nodes+1, :); % weights of edges pointing from input to hidden
+weights_outer = init(hidden_nodes+2:end, :); % weights of edges pointing from hidden to output 
+weights_outer(isnan(weights_outer)) = []; 
 
 while (m < epoch)
    for t = 1:n
@@ -9,24 +16,25 @@ while (m < epoch)
       
        %For hidden layer
        a_1 = [-1; a_1']; 
-       in_1 = weights_1*a_1; 
-       a_2 = sigmoid(in_1); 
+       in_i = weights_inner*a_1; 
+       a_2 = sigmoid(in_i); 
       
        %For output layer
        a_2 = [1; a_2]; 
-       in_2 = weights_2*a_2; 
-       a_3 = sigmoid(in_2); 
+       in_j = weights_outer*a_2; 
+       a_3 = sigmoid(in_j); 
        
-       delta_2 = sigmoid_prime(in_2).*(y(t)-a_3); 
+       delta_j = sigmoid_prime(in_j)*(y(t)-a_3); 
        
-       delta_1 = [1; sigmoid_prime(in_1)]*sum(sum(weights_1))*delta_2; 
+       delta_i = [1; sigmoid_prime(in_i)]*sum(weights_outer*delta_j); 
        
-       weights_1(t, :) = weights_1(t, :) + alpha*a_1'*delta_1; 
-       weights_2(t, :) = weights_2(t, :) + alpha*a_1*delta_2;
-       
+       weights_inner(t, :) = weights_inner(t, :) + alpha*a_1'*delta_i; 
+       weights_outer(t, :) = weights_outer(t, :) + alpha*a_2'*delta_j;
+       a
    end   
     m = m+1; 
-    neural_net = 0; 
+    neural_net(1:hidden_nodes, :) = weights_inner; 
+    neural_net(hidden_nodes+1:end,:) = weights_outer; 
 end
 
 
--- project2-neuralnets/training.m
+++ project2-neuralnets/training.m
@@ -2,17 +2,11 @@ init = importdata('init_neural_net.mat');
 trained = importdata('training_set.mat'); 
 
 input_nodes = init(1,1); % # of input nodes
-hidden_nodes = init(1,2); % # of hidden nodes
-output_nodes = init(1,3); % # of output nodes
-weights_inner = init(2:hidden_nodes+1, :); % weights of edges pointing from input to hidden
-weights_outer = init(hidden_nodes+2, :); % weights of edges pointing from hidden to output 
-weights_outer(isnan(weights_outer)) = []; 
 
 X = trained(:, 1:input_nodes); 
 y = trained(:, input_nodes+1); 
 
-
-neural_net = BackProp(X, y, weights_inner, weights_outer, 100, 0.01); 
+neural_net = BackProp(X, y, init, 100, 0.01); 
 
 
 %B = reshape(data(2:end -1 , :), hidden_nodes, input_nodes); 
