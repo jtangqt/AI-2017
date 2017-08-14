a86ca804fb3e2d6edd38a211619dd478bc9a17a2
jtangqt <tang@cooper.edu>
2017-08-14 16:54:31 -0400

    added user input

--- project2-neuralnets/training.m
+++ project2-neuralnets/training.m
@@ -1,5 +1,14 @@
-init = importfile('init_neural_net.txt'); 
-trained = importfile('training_set.txt'); 
+prompt = 'What is the name of the text file with the initial neural network? ';
+init_neural_net = input(prompt,'s');
+
+prompt = 'What is the name of the text file with the training set? ';
+training_set = input(prompt,'s');
+
+prompt = 'What is the name of the text file for the output? ';
+output_file = input(prompt,'s');
+
+init = importfile(init_neural_net); 
+trained = importfile(training_set); 
 
 input_nodes = init(1,1); % # of input nodes
 
@@ -8,7 +17,7 @@ y = trained(:, input_nodes+1);
 
 neural_net = BackProp(X, y, init, 1, 0.1); 
 
-fid = fopen('my_trained_nn.txt', 'wt'); % Open for writing
+fid = fopen(output_file, 'wt'); % Open for writing
 for i=1:size(neural_net,1)
     for j = 1:size(neural_net, 2)
         if(isnan(neural_net(i, j)))
