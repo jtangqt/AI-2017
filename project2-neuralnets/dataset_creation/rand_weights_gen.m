a37a385cf9ff521da06edb25716b57bdf55863d3
jtangqt <ajasmineflower@gmail.com>
2017-08-21 18:10:31 -0400

    created 5 output nodes

---
+++ project2-neuralnets/dataset_creation/rand_weights_gen.m
@@ -0,0 +1,2 @@
+rng(0, 'twister');
+rand( ,1) 
--- project2-neuralnets/processing_data.m
+++ project2-neuralnets/processing_data.m
@@ -1,25 +1,29 @@
-mat = dlmread('new_test_set.txt');
+mat = dlmread('new_training_set.txt');
 
-new_vector = mat(:, end); 
-output_vector = zeros(size(new_vector, 1), 4); 
+% new_vector = mat(:, 8); 
+% output_vector = zeros(size(new_vector, 1), 5); 
+% 
+% for i = 1:size(new_vector, 1)
+%     if(1.34 <= new_vector(i) && new_vector(i) < 1.36) 
+%         output_vector(i, :) = [1 0 0 0 0];
+%     elseif(1.36 <= new_vector(i) && new_vector(i) < 1.38) 
+%         output_vector(i, :) = [1 1 0 0 0];
+%     elseif(1.38 <= new_vector(i) && new_vector(i) < 1.4) 
+%         output_vector(i, :) = [1 1 1 0 0];
+%     elseif(1.4 <= new_vector(i) && new_vector(i) < 1.42) 
+%         output_vector(i, :) = [1 1 1 1 0];
+%     elseif(1.42 <= new_vector(i) && new_vector(i) < 1.44) 
+%         output_vector(i, :) = [1 1 1 1 1];
+%     end 
+% end
+% 
+% 
+% new_mat = [mat(:, 1:8), output_vector];
 
-for i = 1:size(new_vector, 1)
-    if(1.36 <= new_vector(i) && new_vector(i) < 1.38) 
-        output_vector(i, :) = [1 0 0 0];
-    elseif(1.38 <= new_vector(i) && new_vector(i) < 1.4) 
-        output_vector(i, :) = [1 1 0 0];
-    elseif(1.4 <= new_vector(i) && new_vector(i) < 1.42) 
-        output_vector(i, :) = [1 1 1 0];
-    elseif(1.42 <= new_vector(i) && new_vector(i) < 1.44) 
-        output_vector(i, :) = [1 1 1 1];
-    end 
-end
+mat = [mat(:, 1:7), mat(:, 9:end)]
 
 
-new_mat = [mat, output_vector]
-
-
-fid = fopen('new_test_set.txt', 'wt'); % Open for writing
+fid = fopen('new_training_set.txt', 'wt'); % Open for writing
     for i=1:size(new_mat,1)
         for j = 1:size(new_mat, 2)
             if (j > 8)
